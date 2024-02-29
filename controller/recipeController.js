53026c1de33477aa0c0b2ec98b94d72709e06764
Samiran <samirans170@gmail.com>
2024-02-29 19:51:05 +0530

    create new recipe

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -4,39 +4,37 @@ const cloudinary = require('../middleware/cloudinaryModdleware');
 const createRecipe = async (req, res) => {
     const { recipename, ingredients, description } = req.body;
     if (!recipename || !ingredients || !description) {
-        res.status(404).send({
+        return res.status(400).send({
             status: "Failed",
-            message: "All feilds are required."
+            message: "All fields are required."
         });
-    } else {
+    }
+
+    try {
         let recipeimages;
         const authorId = req.user._id;
-        await cloudinary.uploader.upload(req.file.path, function (err, result) {
-            if (err) {
-                res.status(400).send({
-                    status: "Failed",
-                    message: "File uploaded failed."
-                })
-            } else {
-                recipeimages = result.url;
-            }
+
+        const result = await cloudinary.uploader.upload(req.file.path);
+        recipeimages = result.url;
+
+        const createdRecipe = await Recipe.create({ authorId, recipeimages, recipename, ingredients, description });
+        
+        console.log('Recipe created:', createdRecipe);
+        
+        res.status(201).send({
+            status: "Success",
+            message: "New recipe created",
+            data: createdRecipe
         });
-        try {
-            const createdRecipe = await Recipe.create({ authorId: authorId, recipeimages: recipeimages, recipename: recipename, ingredients: ingredients, description: description });
-            console.log(recipeCount);
-            res.status(201).send({
-                status: "Success",
-                message: "New recipe created",
-                data: createdRecipe
-            })
-        } catch (error) {
-            res.status(400).send({
-                status: "Failed",
-                message: "Post creation error."
-            });
-        }
+    } catch (error) {
+        console.error('Error creating recipe:', error);
+        res.status(500).send({
+            status: "Failed",
+            message: "Failed to create recipe."
+        });
     }
-}
+};
+
 
 module.exports = {
     createRecipe
